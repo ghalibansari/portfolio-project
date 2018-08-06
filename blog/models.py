8513e511fb1df9e362a3b31d8e1f0938ea8b7f48
ghalibansari1994 <ghalibansari1994@gmail.com>
2018-08-06 22:10:12 +0530

    image and summary retrived successfully

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+# Create a blog models
+#title
+#pub_date
+#body
+#image
+
+#Add the blog app to the settings
+
+#create a migration
+
+#migrate
+
+#add to the admin
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
--- jobs/models.py
+++ jobs/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
-    summery = models.CharField(max_length=200)
+    summary = models.CharField(max_length=200)
